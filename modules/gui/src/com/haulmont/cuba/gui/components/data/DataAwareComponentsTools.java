0cad5aaa0b878a47a5697547328f29f0eac7a95f
Denis Pribavkin <pribavkin@haulmont.com>
2019-05-16 13:00:12 +0400

    Support bean validation annotations @FutureOrPresent and @PastOrPresent (1538)

--- modules/gui/src/com/haulmont/cuba/gui/components/data/DataAwareComponentsTools.java
+++ modules/gui/src/com/haulmont/cuba/gui/components/data/DataAwareComponentsTools.java
@@ -38,9 +38,7 @@ import org.springframework.stereotype.Component;
 
 import javax.inject.Inject;
 import javax.persistence.TemporalType;
-import javax.validation.constraints.Future;
-import javax.validation.constraints.Past;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 import java.time.*;
 import java.util.Map;
 import java.util.TimeZone;
@@ -128,6 +126,16 @@ public class DataAwareComponentsTools {
             if (temporalType == TemporalType.DATE) {
                 dateTime = dateTime.plusDays(1);
             }
+            ZonedDateTime zonedDateTime = ZonedDateTime.of(dateTime, ZoneId.systemDefault());
+            //noinspection unchecked
+            component.setRangeStart(dateTimeTransformations.transformFromZDT(zonedDateTime, javaType));
+        } else if (metaProperty.getAnnotations().get(PastOrPresent.class.getName()) != null) {
+            LocalDateTime dateTime = LocalDateTime.of(LocalDate.now(), LocalTime.MAX);
+            ZonedDateTime zonedDateTime = ZonedDateTime.of(dateTime, ZoneId.systemDefault());
+            //noinspection unchecked
+            component.setRangeEnd(dateTimeTransformations.transformFromZDT(zonedDateTime, javaType));
+        } else if (metaProperty.getAnnotations().get(FutureOrPresent.class.getName()) != null) {
+            LocalDateTime dateTime = LocalDateTime.of(LocalDate.now(), LocalTime.MIN);
             ZonedDateTime zonedDateTime = ZonedDateTime.of(dateTime, ZoneId.systemDefault());
             //noinspection unchecked
             component.setRangeStart(dateTimeTransformations.transformFromZDT(zonedDateTime, javaType));
